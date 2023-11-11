41abf0ce3d0782dfa83e8490733c3e79eea49fe5
karthikt1995 <justkarthik@outlook.com>
2023-11-10 18:45:47 -0800

    Commit Commit1 start page with empty contacts

--- karthikt0624/models.py
+++ karthikt0624/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Contact(models.Model):
     serial_number = models.AutoField(primary_key=True)
     contact_name = models.CharField(max_length=255)
+    notes = models.TextField(blank=True, null=True)  # New field for long paragraph
     email = models.EmailField()
     date_created = models.DateTimeField(auto_now_add=True)
 
